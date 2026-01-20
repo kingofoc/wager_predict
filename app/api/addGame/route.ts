e2b5ed7b30fac9e6cabadc405ac0f8a78edc6f3b
kingofoc <kingofoc202@gmail.com>
2026-01-20 14:36:57 +0100

    another commit

--- app/api/addGame/route.ts
+++ app/api/addGame/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDb from "@/lib/mongodb";
 
 export async function POST(request: NextRequest) {
- await connectDb():
+ await connectDb();
 
  const { teamA, teamB } = await request.json();
 
- if(!teamA & !teamB) {
+ if(!teamA && !teamB) {
   return NextResponse.json({ error: "Please input teams" }, { status: 400 });
  }
 
